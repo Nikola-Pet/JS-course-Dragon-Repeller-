aed34597e3b0f2fcd1e78ad4f4d1491ff2b2712c
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-04 00:50:09 +0100

    Step 21

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-let button1 = document.querySelector('#button1');
+
+const button1 = document.querySelector('#button1');
