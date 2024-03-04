8c0fd1b2d10122fdbbb9c2b6616101228ac6a5d7
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-04 18:36:24 +0100

    Step 53

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const locations = [{}];
+const locations = [{name: "town square"}];
 
 //initialize buttons
 button1.onclick = goStore;
