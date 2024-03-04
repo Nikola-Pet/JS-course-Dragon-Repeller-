e805e0bbeeb8615094d79d7fafa18b86bdae359f
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-04 19:43:47 +0100

    Step 67

--- script.js
+++ script.js
@@ -45,8 +45,9 @@ function update(location) {
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
-    
+
     text.innerText = location.text;
+    
 }
 
 function goTown() {
