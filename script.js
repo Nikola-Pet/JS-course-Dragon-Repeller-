c74b0a71b19fa875a5a3d05a74d420304246ef3a
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-05 18:49:18 +0100

    Step 126

--- script.js
+++ script.js
@@ -172,7 +172,9 @@ function attack() {
     }
 }
 
-function dodge() {}
+function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
 function defeatMonster() {}
 
