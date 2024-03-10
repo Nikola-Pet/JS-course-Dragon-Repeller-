fb8dc916d1d144c10c048af6100194617b9b481a
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-10 20:14:11 +0100

    Step 146

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ function getMonsterAttackValue(level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
     console.log(hit)
 
-    return hit;
+    return hit > 0 ? hit : 0;
 }
 
 function dodge() {
