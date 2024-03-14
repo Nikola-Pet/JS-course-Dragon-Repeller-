813a28bfe14209b3a081fdf2ade7113bcc5e8b9e
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-14 17:42:15 +0100

    Step 167

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ function pick(guess) {
 
   text.innerText = "You picked "+ guess +". Here are the random numbers:\n";
 
-  for (let i = 0; x < 5; i++) {
+  for (let i = 0; i < 5; i++) {
 
   }
 }
