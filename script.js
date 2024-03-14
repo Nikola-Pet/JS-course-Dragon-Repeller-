6124db06d9ce1b074d7edde7c7acfbc6e3d8bb1b
Nikola-Pet <pet.nikola97@gmail.com>
2024-03-14 17:34:03 +0100

    Step 164

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ function pick(guess) {
     numbers.push(Math.floor(Math.random() * 11));
   }
 
-  text.innerText = "You picked "+ guess +". Here are the random numbers:";
+  text.innerText = "You picked "+ guess +". Here are the random numbers:\n";
 }
 
 function pickTwo() {
